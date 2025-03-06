53435063a8cd74936cc889ba6b0298551171628a
Varvara M. <mvd2048@gmail.com>
2025-03-06 13:03:10 +0300

    clearing main branch
    перегрузила папку по-другому, чтобы не создавать путаницу и не оборачивать репозитории лишний раз

--- Apart_app/project-template/src/components/HomePage/Contacts.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Contacts = () => {
-  return <div>Контакты</div>;
-};
-
-export default Contacts;
--- Apart_app/project-template/src/components/HomePage/Offers.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Offers = () => {
-  return <div>Секция выгодных предложений</div>;
-};
-
-export default Offers;
