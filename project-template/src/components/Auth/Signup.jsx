77f0b7ab9f06066a71fb1b8822bdfbe69da24893
Varvara M. <mvd2048@gmail.com>
2025-04-07 13:11:44 +0300

    Signup page changed

--- project-template/src/components/Auth/Signup.jsx
+++ project-template/src/components/Auth/Signup.jsx
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { auth } from "../../firebase";
 
+import styles from '../../styles/signup.module.css';
+
 const Signup = () => {
   const [username, setUsername] = useState("");
+  const [lastname, setLastname] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [copypassword, setCopyPassword] = useState("");
@@ -13,6 +16,7 @@ const Signup = () => {
     e.preventDefault();
 
     /* проверка что пароль и копия пароля совпадает */
+
     if(copypassword !== password) {
       setError("Passwords don't match, try again please")
       return
@@ -22,12 +26,17 @@ const Signup = () => {
       .then ((user) => {
         /* при успешном создании аккаунта очищаем поля */
         console.log('пользователь успешно создан');
+
         setError("");
+        setUsername("");
         setLastname("");
         setEmail("");
         setPassword("");
         setCopyPassword("");
+
         updateProfile(auth.currentUser, {displayName: username});
+        updateProfile(auth.currentUser, {lastname: lastname});
+
         console.log(lastname);
       })
       .catch((error)=>console.log(error));
@@ -35,47 +44,85 @@ const Signup = () => {
 
   return (
     <div>
-      <h1>Создать аккаунт</h1>
-      {/* Форма регистрации */}
 
-      <div>
-        <form onSubmit={register}>
-          <input 
-            placeholder='Enter your firstname'
-            value={username} 
-            onChange={(e)=>setUsername(e.target.value)} 
-            type="text" 
+        {/* форма регистрации */}
+        <form onSubmit={register} className={styles.container}>
+        
+        {/* левая половина формы */}
+        
+        <div className={styles.containerLeft}>
 
-            minlength="2"
-            maxlength="12"
-            size="14"
-            required  />
-          
-          <input 
-            placeholder='Enter your email'
-            value={email} 
-            onChange={(e)=>setEmail(e.target.value)} 
-            type="email" 
-            required  />
+          <div className={styles.header}>
+            <h1 className={styles.pageHeader}>Time to take</h1>
+            <h2 className={`${styles.pageHeader} ${styles.extra}`}>first steps towards your future!</h2>
+          </div>
+        
+          <div className={`${styles.infoBlock} ${styles.personal}`}>
+            <input 
+              className={styles.input}
+              placeholder='Enter your firstname'
+              value={username} 
+              onChange={(e)=>setUsername(e.target.value)} 
+              type="text" 
 
-          <input 
-            placeholder='Enter your password'
-            value={password} 
-            onChange={(e)=>setPassword(e.target.value)}
-            type="password"
-            required  />
+              minlength="2"
+              maxlength="12"
+              size="14"
+              required  />
 
             <input 
+              className={styles.input}
+              placeholder='Enter your lastname'
+              value={lastname} 
+              onChange={(e)=>setLastname(e.target.value)} 
+              type="text" 
+
+              minlength="2"
+              maxlength="12"
+              size="14"
+              required  />
+            
+            <input 
+              className={styles.input}
+              placeholder='Enter your email'
+              value={email} 
+              onChange={(e)=>setEmail(e.target.value)} 
+              type="email" 
+              required  />
+          </div>
+
+          <div className={styles.infoBlock}>
+            <span className={styles.inputLabel}>Create a strong password!</span>
+            <input 
+              className={styles.input}
+              placeholder='Enter your password'
+              value={password} 
+              onChange={(e)=>setPassword(e.target.value)}
+              type="password"
+              required  />
+
+            <input 
+              className={styles.input}
               placeholder='Repeat the password'
               value={copypassword} 
               onChange={(e)=>setCopyPassword(e.target.value)}
               type="password"
               required />
+          </div>
+        </div>
 
-            <button>Create</button>
+        {/* правая половина формы */}
+
+        <div className={styles.containerRight}>
+
+          <div className={styles.content}>
+            <button className={styles.createButton}>Create</button> 
             {error ? <p style={{color:'red'}}>{error}</p> : ""}
-        </form>
-      </div>
+          </div>
+
+        </div>
+        
+      </form>
     </div>
   );
 };
