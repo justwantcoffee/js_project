41f715e9156cbf58ac0f75797b6a5e6117b190e9
Zeroday456 <80755427+Zeroday456@users.noreply.github.com>
2025-05-22 01:36:33 +0300

    Update LandlordCabinet.jsx
    Add link to AddApartment page to LandLordContent.

--- project-template/src/components/ProfileContent/LandlordCabinet.jsx
+++ project-template/src/components/ProfileContent/LandlordCabinet.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { UserCard } from '../Profile/UserCard';
 import { ClientList } from '../Profile/ClientList';
@@ -6,15 +7,21 @@ import { Published } from '../Profile/Published';
 
 import styles from '../../styles/Profile/profile.module.css';
 
-
 const LandlordCabinet = () => {
   return (
     <div className={styles.cards}>
       {/* Кабинет арендодателя/продавца */}
+
       
+
       <UserCard />
       <ClientList />
       <Published />
+      <div style={{ marginBottom: "20px" }}>
+        <Link to="/add-apartment" className={styles.addButton}>
+          ➕ Создать объявление
+        </Link>
+      </div>
     </div>
   );
 };
